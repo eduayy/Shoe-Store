d0bfdc06ce9c2d6bbe89f3ee37b0d4b03c38a59a
eduayy <cesr031223@gmail.com>
2025-05-12 12:02:20 -0600

    new design for login

--- Frontend/src/auth/Login/Login.jsx
+++ Frontend/src/auth/Login/Login.jsx
@@ -3,18 +3,19 @@ import { useState } from "react";
 
 function Login() {
   const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
   const [message, setMessage] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:3001/api/users", {
+      const response = await fetch("http://localhost:3001/api/users/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email }),
+        body: JSON.stringify({ userName, email }),
       });
 
       if (response.ok) {
@@ -31,19 +32,39 @@ function Login() {
 
   return (
     <main className="main-content">
-      <h1>Iniciar sesión</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Correo electrónico:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <button type="submit">Entrar</button>
-      </form>
-      {message && <p className="welcome-message">{message}</p>}
+      <section className="form-section">
+        <div className="login-header">
+          <h1>Login</h1>
+          <p>Welcome back to our app!</p>
+        </div>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <label className="label-login" htmlFor="user-name">
+            UserName:
+          </label>
+          <input
+            className="form-input-login"
+            type="text"
+            id="userName"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          ></input>
+          <label className="label-login" htmlFor="email">
+            Email:
+          </label>
+          <input
+            className="form-input-login"
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <button className="btn-submit" type="submit">
+            Entrar
+          </button>
+        </form>
+        {message && <p className="welcome-message">{message}</p>}
+      </section>
     </main>
   );
 }
